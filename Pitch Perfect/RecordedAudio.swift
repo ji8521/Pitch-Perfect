a7d68f566c97ec7a017fa28c1ed6dc8dead73964
ji8521 <ji8521@gmail.com>
2015-11-03 23:25:56 -0500

    Refactored for Swift 2

--- Pitch Perfect/RecordedAudio.swift
+++ Pitch Perfect/RecordedAudio.swift
@@ -8,8 +8,14 @@
 
 import Foundation
 
+//Our class used to save the audio file attributes: Url of the path and the title.
 class RecordedAudio: NSObject{
-    
     var filePathUrl: NSURL!
     var title: String!
+    
+    init(filePathUrl:NSURL,title:String){
+        self.filePathUrl = filePathUrl
+        self.title = title
+        super.init()
+    }
 }
